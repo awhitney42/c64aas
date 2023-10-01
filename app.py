a97d5501f939012972a47f5e98fd2ae36d6b6525
Adam Whitney <adam@adamwhitney.net>
2023-10-01 09:48:39 -0500

    Update app.py to bind to port 6464 instead of 8000.

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=6464, debug=True)
 
